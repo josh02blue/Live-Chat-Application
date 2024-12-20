b9e4af9e834e5f726163b92f5906f5755be17931
Joshua <joshuaholguin0611@gmail.com>
2024-12-20 02:22:25 -0500

    Completed Login and Signup Pages

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -14,6 +15,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
